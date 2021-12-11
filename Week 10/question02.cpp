454c2d46139cb2b21b0bdcf363641e2f1635bd75
SHEKHAR SAXENA <shekhar.happy77@gmail.com>
2021-12-12 02:05:53 +0530

    Create question02.cpp

---
+++ Week 10/question02.cpp
@@ -0,0 +1,74 @@
+// Shekhar Saxena
+// Section: A
+// Class Roll No: 49
+// University Roll No: 2014852
+// Subject: Week 10 Q1
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+    if (a.first == b.first)
+        return a.second < b.second;
+    else
+        return a.first < b.first;
+}
+int check_index(int a[], int b[], int i, int j, bool complete[], int n)
+{
+    for (int k = 0; k < n; k++)
+    {
+        if (a[k] == i && b[k] == j && !complete[k])
+        {
+            complete[k] = true;
+            return k + 1;
+        }
+    }
+}
+void solve()
+{
+    int n, i;
+    cin >> n;
+    vector<pair<int, int>> v;
+    int tt[n], e[n];
+    for (i = 0; i < n; i++)
+        cin >> tt[i];
+    for (i = 0; i < n; i++)
+        cin >> e[i];
+    for (i = 0; i < n; i++)
+        v.push_back(make_pair(e[i], tt[i]));
+    vector<int> t;
+    sort(v.begin(), v.end());
+    int count = 0, j = 0;
+    bool comp[n] = {false};
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i].first - j >= v[i].second)
+        {
+            j = j + v[i].second;
+            count++;
+            t.push_back(check_index(e, tt, v[i].first, v[i].second, comp, n));
+        }
+    }
+    cout << "Max number of task: " << count;
+    cout << "\nSelected task no: ";
+    for (int i = 0; i < t.size(); i++)
+    {
+        cout << t.at(i) << ",";
+    }
+}
+int main()
+{
+// Setting up Input/ Output
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    // Driver Code
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
