991af17335357e24c8174c7755edbebbc819fc2a
SHEKHAR SAXENA <shekhar.happy77@gmail.com>
2021-12-12 02:09:38 +0530

    Create question03.cpp

---
+++ Week 09/question03.cpp
@@ -0,0 +1,50 @@
+// Shekhar Saxena
+// Section: A
+// Class Roll No: 49
+// University Roll No: 2014852
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 9999999
+#define ll long long int
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    priority_queue<int, vector<int>, greater<int>> minheap;
+    for (int i = 0; i < n; i++)
+    {
+        minheap.push(a[i]);
+    }
+    int ans = 0;
+    while (minheap.size() > 1)
+    {
+        int e1 = minheap.top();
+        minheap.pop();
+        int e2 = minheap.top();
+        minheap.pop();
+        ans += e1 + e2;
+        minheap.push(e1 + e2);
+    }
+    cout << ans;
+}
+int main()
+{
+// Setting up Input/ Output
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    // Driver Code
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
